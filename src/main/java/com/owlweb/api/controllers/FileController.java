729e9483a0719d87a826c736d72077b3f6e37e5c
Aidas Jankauskas <aidas.jankauskas31@gmail.com>
2023-04-25 16:45:40 +0300

    Initial commit

---
+++ src/main/java/com/owlweb/api/controllers/FileController.java
@@ -0,0 +1,35 @@
+package com.owlweb.api.controllers;
+
+import com.owlweb.api.services.FileConvertService;
+import com.owlweb.api.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileController {
+    private final StorageService storageService;
+    private final FileConvertService fileConvertService;
+
+    @Autowired
+    public FileController(StorageService storageService, FileConvertService fileConvertService) {
+        this.storageService = storageService;
+        this.fileConvertService = fileConvertService;
+    }
+
+    @PostMapping("/api/convert")
+    public ResponseEntity<Resource> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        storageService.store(file);
+
+        String convertedFileName = this.fileConvertService.convertFile(file.getOriginalFilename(), "rdf");
+
+        Resource convertedFile = storageService.loadAsResource(convertedFileName);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + convertedFileName + "\"").body(convertedFile);
+    }
+}
---
+++ src/main/java/com/owlweb/api/services/FileConvertService.java
@@ -0,0 +1,12 @@
+package com.owlweb.api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileConvertService {
+    public String convertFile(String sourceFileName, String destinationFormat) {
+        // Convert file to another format
+        // Return converted file name
+        return sourceFileName;
+    }
+}
