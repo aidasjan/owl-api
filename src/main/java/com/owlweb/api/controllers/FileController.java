f8f80c6d098a3aa88800dffb97e80b299dacccac
Aidas Jankauskas <aidas.jankauskas31@gmail.com>
2023-05-05 02:09:35 +0300

    Cross origin

--- src/main/java/com/owlweb/api/controllers/FileController.java
+++ src/main/java/com/owlweb/api/controllers/FileController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class FileController {
     private final StorageService storageService;
