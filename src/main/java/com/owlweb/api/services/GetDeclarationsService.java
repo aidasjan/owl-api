638776646e9154733d19ee91396b8034ca01e128
Matus Hedera <matus.hedera9@gmail.com>
2023-05-07 11:49:41 +0200

    Get declarations service

---
+++ src/main/java/com/owlweb/api/services/GetDeclarationsService.java
@@ -0,0 +1,24 @@
+package com.owlweb.api.services;
+
+import org.springframework.stereotype.Service;
+import org.apache.commons.io.FilenameUtils;
+import sk.matfyz.owltoolkit.OWLDeclarations;
+
+@Service
+public class GetDeclarationsService {
+	public String getDeclarations(String sourceFileName) {
+
+		String destinationFileName = "declarations_"+sourceFileName;
+		try {
+
+			String bareFileName = FilenameUtils.removeExtension(sourceFileName);
+
+			OWLDeclarations.main("uploads/"+sourceFileName, "uploads/"+destinationFileName);
+
+		} catch (Exception e) {
+			System.out.println("Exception occured while getting declarations.");
+		}
+
+		return destinationFileName;
+	}
+}
